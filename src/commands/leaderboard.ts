5613a49d908c8558fd2b2cd5ba995dd233d59679
EdgarSaldivar <edgarls93@hotmail.com>
2022-12-30 02:23:10 -0500

    Leaderboard fix

--- src/commands/leaderboard.ts
+++ src/commands/leaderboard.ts
@@ -36,7 +36,7 @@ module.exports = {
           ),
         );
       }
-      virginArray.sort((a, b) => (a.virginity > b.virginity ? -1 : 1));
+      virginArray.sort((a, b) => a.virginity - b.virginity);
       for (let i = 0; i < virginArray.length; i++) {
         boardEmbed.addFields({
           name: i + 1 + ') ' + virginArray[i].username,
