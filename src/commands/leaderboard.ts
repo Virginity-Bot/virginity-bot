29ec67ffbf271b8613080cd75e1d09d43cb7a75e
EdgarSaldivar <edgarls93@hotmail.com>
2023-01-04 20:44:05 -0800

    .

--- src/commands/leaderboard.ts
+++ src/commands/leaderboard.ts
@@ -69,6 +69,7 @@ module.exports = {
       }
       await interaction.reply({ embeds: [boardEmbed] });
     } catch (e) {
+      console.log(e);
       await interaction.reply('No Virgins :(');
     }
   },
