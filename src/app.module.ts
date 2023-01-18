8da0531ff1b03ad2816c579e7b7372d352cddc16
Louis Orleans <louis@orleans.io>
2023-01-18 01:54:58 -0800

    ⚰️ remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,8 @@
 import { Module } from '@nestjs/common';
-import { DiscordModule } from '@discord-nestjs/core';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { ConfigModule } from '@nestjs/config';
-import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 
-import { DiscordConfigService } from './bot/discord-config.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BotModule } from './bot/bot.module';
-import { VCEvent } from './entities/vc-event.entity';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
