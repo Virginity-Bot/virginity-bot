716c5cd0e49d7135c922690adbc20c1f180db183
Louis Orleans <louis@orleans.io>
2023-01-18 01:55:18 -0800

    🚧 attempt to init the DB

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { MikroORM } from '@mikro-orm/core';
 import configuration from './config/configuration';
 
 dotenv.config();
@@ -8,6 +9,23 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
+
+  const orm = app.get(MikroORM);
+  // TODO: do we actually want to do this in prod?
+  await setup_db(orm);
+
   await app.listen(configuration.port);
 }
 bootstrap();
+
+/**
+ * Creates initial database schema if none exists.
+ */
+async function setup_db(orm: MikroORM) {
+  const schema_gen = orm.getSchemaGenerator();
+
+  await schema_gen.execute(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
+  // creates an empty schema?
+  await schema_gen.ensureDatabase();
+}
