cfd83b62d47dbcf116ddce0d62b83697da064436
Louis Orleans <louis@orleans.io>
2023-01-17 23:52:30 -0800

    ✨ allow configuration of port

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import configuration from './config/configuration';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
-  await app.listen(3000);
+  await app.listen(configuration.port);
 }
 bootstrap();
