91cee3c472ece7e3111407d74fce517b4c2ca3fe
Louis Orleans <louis@orleans.io>
2023-01-18 01:53:22 -0800

    🔊 configure MikroORM to log with Nest

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,13 +1,16 @@
 import { Options } from '@mikro-orm/core';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { Logger } from '@nestjs/common';
 
 import configuration from './config/configuration';
 import { Virgin } from './entities/virgin.entity';
 import { Guild } from './entities/guild.entity';
 import { VCEvent } from './entities/vc-event.entity';
 
+const logger = new Logger('MikroORM');
 const config: Options = {
   metadataProvider: TsMorphMetadataProvider,
+  logger: Logger.log.bind(logger),
 
   entities: [Virgin, Guild, VCEvent],
 
