6c7759e089f24480df9d834abe8e70f94b9af174
Louis Orleans <louis@orleans.io>
2023-01-18 22:16:41 -0800

    ♻️ use single bot module

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,17 +1,26 @@
 import { DiscordModule } from '@discord-nestjs/core';
 import { Module } from '@nestjs/common';
 import { DiscordConfigService } from 'src/bot/discord-config.service';
+import { DatabaseModule } from 'src/database/database.module';
+import { LeaderboardCommand } from './commands/leaderboard.command';
 
-import { SlashCommandsModule } from './commands/slash-commands.module';
-import { EventsModule } from './events/events.module';
+import { CreateWitnessedGuilds } from './events/create-witnessed-guilds';
+import { IntroMusic } from './events/intro-music';
+import { Track } from './events/track';
 
 @Module({
   imports: [
     DiscordModule.forRootAsync({
       useClass: DiscordConfigService,
     }),
-    SlashCommandsModule,
-    EventsModule,
+    DatabaseModule,
+  ],
+  providers: [
+    LeaderboardCommand,
+
+    IntroMusic,
+    Track,
+    CreateWitnessedGuilds,
   ],
 })
 export class BotModule {}
--- src/bot/commands/slash-commands.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LeaderboardCommand } from './leaderboard.command';
-import { DatabaseModule } from 'src/database/database.module';
-
-@Module({
-  imports: [DatabaseModule],
-  providers: [LeaderboardCommand],
-})
-export class SlashCommandsModule {}
