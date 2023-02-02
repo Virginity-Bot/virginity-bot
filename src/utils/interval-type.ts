9f8490196f592a9a4c311f7bfe022b53dcba9057
Louis Orleans <louis@orleans.io>
2023-02-01 19:14:03 -0800

    🐛 fix type parsing

--- src/utils/interval-type.ts
+++ src/utils/interval-type.ts
@@ -26,44 +26,22 @@ export class IntervalType extends Type<number, string> {
       throw ValidationError.invalidType(IntervalType, value_ms, 'JS');
     }
 
-    return `INTERVAL '${value_ms} ms'`;
+    return `'${value_ms} ms'`;
   }
 
   convertToDatabaseValueSQL(key: string, platform: Platform): string {
-    return `INTERVAL '${key} ms'`;
+    return `INTERVAL ${key}`;
   }
 
   /**
-   * @param value The DB value as a string.
+   * @param value_ms The DB value as a string.
    */
-  convertToJSValue(value: string | number, platform: Platform): number {
-    if (typeof value !== 'string') {
-      throw ValidationError.invalidType(IntervalType, value, 'DB');
+  convertToJSValue(value_ms: string | number, platform: Platform): number {
+    if (typeof value_ms !== 'string') {
+      throw ValidationError.invalidType(IntervalType, value_ms, 'DB');
     }
 
-    /**
-     * PostgresQL supports multiple output styles for its intervals. You can
-     * check which one you're using by checking the value of `intervalstyle` in
-     * your DB.
-     *
-     * The 4 possible styles are `sql_standard`, `postgres`, `postgres_verbose`,
-     * and `iso_8601`, with `postgres` being the default.
-     *
-     * We need to handle all 4.
-     */
-
-    const { years, months, days, hours, minutes, seconds, milliseconds } =
-      this._parseDBIntervalStyle(value);
-
-    return new Date(
-      years,
-      months,
-      days,
-      hours,
-      minutes,
-      seconds,
-      milliseconds,
-    ).getTime();
+    return parseInt(value_ms);
   }
 
   convertToJSValueSQL(key: string, platform: Platform): string {
@@ -80,68 +58,5 @@ export class IntervalType extends Type<number, string> {
 
   compareAsType(): string {
     return 'number';
-  }
-
-  _parseDBIntervalStyle(value: string): {
-    years: number;
-    months: number;
-    days: number;
-    hours: number;
-    minutes: number;
-    seconds: number;
-    milliseconds: number;
-  } {
-    let matches: RegExpMatchArray | null;
-
-    if (
-      // checks for `postgres`:
-      // 6 years 5 mons 4 days 03:02:01
-      /^\d /.test(value)
-    ) {
-      matches = value.match(regex_interval_style_postgres);
-    } else if (
-      // checks for `sql_standard`:
-      // +6-5 +4 +3:02:01
-      value.startsWith('+')
-    ) {
-      matches = value.match(regex_interval_style_sql_standard);
-    } else if (
-      // checks for `iso_8601`:
-      // P6Y5M4DT3H2M1S
-      value.startsWith('P')
-    ) {
-      matches = value.match(regex_interval_style_iso_8601);
-    } else if (
-      // checks for `postgres_verbose`:
-      // @ 6 years 5 mons 4 days 3 hours 2 mins 1 sec
-      value.startsWith('@ ')
-    ) {
-      matches = value.match(regex_interval_style_postgres_verbose);
-    } else {
-      throw ValidationError.invalidType(IntervalType, value, 'DB');
-    }
-
-    return {
-      years:
-        matches?.groups?.years != null ? parseFloat(matches?.groups?.years) : 0,
-      months:
-        matches?.groups?.months != null
-          ? parseFloat(matches?.groups?.months)
-          : 0,
-      days:
-        matches?.groups?.days != null ? parseFloat(matches?.groups?.days) : 0,
-      hours:
-        matches?.groups?.hours != null ? parseFloat(matches?.groups?.hours) : 0,
-      minutes:
-        matches?.groups?.minutes != null
-          ? parseFloat(matches?.groups?.minutes)
-          : 0,
-      seconds:
-        matches?.groups?.seconds != null
-          ? parseFloat(matches?.groups?.seconds)
-          : 0,
-      milliseconds:
-        matches?.groups?.ms != null ? parseFloat(matches?.groups?.ms) : 0,
-    };
   }
 }
