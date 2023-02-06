7f6c1f14461f70356d45002452cab111c68dc97f
EdgarSaldivar <edgarls93@hotmail.com>
2023-02-05 20:21:51 -0800

    🐛 Fixed Configuration
    Co-authored-by: Louis Orleans <louis@orleans.io>

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,16 @@
 /* eslint no-process-env: "off" */
+/* eslint no-console: "off" */
 
 import { minutesToMilliseconds } from 'date-fns';
+import {
+  IsEnum,
+  validate,
+  IsInt,
+  Min,
+  Max,
+  IsString,
+  Matches,
+} from 'class-validator';
 
 // TODO: switch to using @nestjs/config
 
@@ -11,13 +21,16 @@ export enum LogLevel {
   DEBUG,
 }
 
-const configuration = {
-  log_level:
+class Configuration {
+  @IsEnum(LogLevel, {
+    message: `$property must be one of QUIET, ERROR, WARN, DEBUG`,
+  })
+  log_level =
     process.env.LOG_LEVEL != null
       ? LogLevel[process.env.LOG_LEVEL]
-      : LogLevel.WARN,
+      : LogLevel.WARN;
 
-  db: {
+  db = {
     type: (process.env.DATABASE_TYPE ?? 'postgresql') as
       | 'postgresql'
       | 'mongo'
@@ -31,13 +44,13 @@ const configuration = {
       max: parseInt(process.env.DATABASE_POOL_MAX ?? '5'),
     },
     auto_migrate: (process.env.DATABASE_AUTO_MIGRATE ?? 'true') === 'true',
-  },
+  };
 
-  mikro_orm: {
+  mikro_orm = {
     cache_dir: process.env.MIKRO_ORM_CACHE_DIR ?? `${process.cwd()}/temp`,
-  },
+  };
 
-  storage: {
+  storage = {
     s3: {
       host: process.env.STORAGE_S3_HOST as string,
       port: parseInt(process.env.STORAGE_S3_PORT as string),
@@ -53,22 +66,45 @@ const configuration = {
         parseFloat(process.env.STORAGE_AUDIO_MAX_FILE_SIZE_KiB ?? '1024') *
         1024,
     },
-  },
+  };
 
-  port: parseInt(process.env.PORT ?? '3000'),
+  @IsInt()
+  @Min(1)
+  @Max(65535)
+  port = parseInt(process.env.PORT ?? '3000');
 
-  discord_token: process.env.DISCORD_TOKEN as string,
+  @IsString()
+  @Matches(/^\S+$/)
+  discord_token = process.env.DISCORD_TOKEN as string;
 
-  score: {
+  score = {
     reset_schedule: process.env.SCORE_RESET_SCHEDULE ?? '0 2 * * Tue',
-  },
+  };
 
-  audio: {
+  audio = {
     default_intro: {
       path: 'assets/entrance_theme.opus',
       timeout_ms: minutesToMilliseconds(4.9),
     },
-  },
-};
+  };
+}
+
+const configuration = new Configuration();
+validate(configuration)
+  .then((errs) => {
+    console.error(
+      errs
+        .map((err) =>
+          err.constraints != null
+            ? Object.values(err.constraints)
+                .map((m) => `${m}.`)
+                .join('\n')
+            : `Check ${err.property}.`,
+        )
+        .join('\n'),
+    );
+    process.exit(1);
+  })
+  .catch((err) => console.error(err));
 
 export default configuration;
