66d206f5e80ff5eaa6f6d4b0490bdd68f8b5534c
Louis Orleans <louis@orleans.io>
2023-02-06 17:34:00 -0800

    ⚡️✨ version container images & improve container build times & sizes (#89)

---
+++ .github/workflows/scripts/get-image-tags.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+GITHUB_REPOSITORY="$1"
+GITHUB_REF_NAME="$2"
+GITHUB_REF_TYPE="$3"
+GITHUB_EVENT_REPOSITORY_DEFAULT_BRANCH="$4"
+
+container_repo="ghcr.io/$(echo "$GITHUB_REPOSITORY" | tr '[A-Z]' '[a-z]')"
+versions="$(
+  echo "$GITHUB_REF_NAME" \
+  | tr '[A-Z]' '[a-z]' \
+  | perl -ne 's/[^a-z0-9._\n]+/-/g; print' \
+)"
+if [[ "$GITHUB_REF_TYPE" == "branch" ]]; then
+  package_json_version="$(jq --raw-output '.version' 'package.json')"
+  versions="$versions,$package_json_version"
+fi
+
+# Use Docker `latest` tag convention, only tagging `latest` on default branch.
+if [[ "$versions" =~ ,?$GITHUB_EVENT_REPOSITORY_DEFAULT_BRANCH,? ]]; then
+  versions="$versions,latest"
+fi
+
+echo "$versions"
+
+versions=$(echo $versions | tr ',' '\n')
+image_tags="bot"
+for version in $versions; do
+  image_tags="$image_tags,$container_repo/bot:$version"
+done
+
+echo "image_tags=$image_tags" >> "$GITHUB_OUTPUT"
