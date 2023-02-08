881feca40643d5c433bb5ad56775aee3dccc3a37
Louis Orleans <louis@orleans.io>
2023-02-08 03:18:26 -0800

    ✨ allow users to select pre-existing intros

--- src/bot/commands/settings.command.ts
+++ src/bot/commands/settings.command.ts
@@ -3,29 +3,38 @@ import {
   Command,
   EventParams,
   Handler,
+  InjectDiscordClient,
   InteractionEvent,
+  On,
   Param,
   ParamType,
 } from '@discord-nestjs/core';
 import { SlashCommandPipe } from '@discord-nestjs/common';
 import {
+  AutocompleteInteraction,
   CommandInteraction,
+  Events,
   MessagePayload,
   PermissionFlagsBits,
+  Client,
+  ApplicationCommandOptionChoiceData,
 } from 'discord.js';
 import { MikroORM, UseRequestContext } from '@mikro-orm/core';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { EntityRepository } from '@mikro-orm/postgresql';
-import { Duration, formatDuration } from 'date-fns';
+import { Duration, formatDuration, secondsToMilliseconds } from 'date-fns';
 
+import configuration from 'src/config/configuration';
 import { GuildEntity } from 'src/entities/guild';
 import { VirginEntity } from 'src/entities/virgin.entity';
+import { IntroSongEntity } from 'src/entities/intro-song.entity';
 import { pascal_spaces, possess } from 'src/utils/string-transformers';
 import { SettingsService, UserFacingError } from './settings.service';
 import {
   GuildAdminIfParam,
   GuildAdminIfParamGuard,
 } from '../guards/guild-admin-if-param.guard';
+import { IsAutocompleteInteractionGuard } from '../guards/is-autocomplete-interaction.guard';
 
 const intro_song_file = 'intro_song_file';
 
@@ -38,6 +47,15 @@ export class SettingsDTO {
     type: ParamType.USER,
   })
   virgin_to_modify?: string;
+
+  @Param({
+    name: 'intro_song',
+    description: 'Pick from intro songs other users have uploaded.',
+    required: false,
+    type: ParamType.STRING,
+    autocomplete: true,
+  })
+  intro_song_hash: string;
 
   /** Attachment snowflake */
   @Param({
@@ -79,7 +97,11 @@ export class SettingsCommand {
     private readonly virgins: EntityRepository<VirginEntity>,
     @InjectRepository(GuildEntity)
     private readonly guilds: EntityRepository<GuildEntity>,
+    @InjectRepository(IntroSongEntity)
+    private readonly intro_songs: EntityRepository<IntroSongEntity>,
     private readonly settings: SettingsService,
+    @InjectDiscordClient()
+    private readonly client: Client,
   ) {}
 
   @Handler()
@@ -116,7 +138,47 @@ export class SettingsCommand {
       id: interaction.guild.id,
     });
 
-    if (dto.intro_song_file_snowflake != null) {
+    if (dto.intro_song_hash != null) {
+      if (!guild_ent.intro.custom_enabled) {
+        messages.push(`Custom intro songs are disabled in this server.`);
+      } else {
+        const intro_song_ent = await this.intro_songs.findOne({
+          hash: dto.intro_song_hash,
+          public: true,
+        });
+        if (intro_song_ent == null) {
+          messages.push('Could not find the requested song.');
+        } else if (
+          intro_song_ent.duration_ms >
+          secondsToMilliseconds(guild_ent.intro.max_duration_s)
+        ) {
+          messages.push('Selected song it too long.');
+        } else {
+          target_user.intro_song = intro_song_ent;
+          await this.virgins.flush();
+
+          const intro_song_timeout = new Date(
+            intro_song_ent.computed_timeout_ms,
+          );
+          const duration: Duration = {
+            years: intro_song_timeout.getUTCFullYear() - 1970,
+            months: intro_song_timeout.getUTCMonth(),
+            days: intro_song_timeout.getUTCDate() - 1,
+            hours: intro_song_timeout.getUTCHours(),
+            minutes: intro_song_timeout.getUTCMinutes(),
+            seconds: intro_song_timeout.getUTCSeconds(),
+          };
+
+          messages.push(
+            `Intro song updated. ${pascal_spaces(
+              target_user_name,
+            )} intro cool-down will now be ${formatDuration(duration, {
+              delimiter: ', ',
+            })}.`,
+          );
+        }
+      }
+    } else if (dto.intro_song_file_snowflake != null) {
       if (!guild_ent.intro.custom_enabled) {
         messages.push(`Custom intro songs are disabled in this server.`);
       } else {
@@ -182,12 +244,47 @@ export class SettingsCommand {
           )} intro song has been reset to default.`,
         );
       }
-
-      interaction.followUp(
-        new MessagePayload(interaction.channel, {
-          content: messages.join('\n'),
-        }),
-      );
     }
+
+    interaction.followUp(
+      new MessagePayload(interaction.channel, {
+        content: messages.join('\n'),
+      }),
+    );
+  }
+
+  @On(Events.InteractionCreate)
+  @UseGuards(IsAutocompleteInteractionGuard)
+  @UseRequestContext()
+  async autocomplete(interaction: AutocompleteInteraction): Promise<void> {
+    if (interaction.member == null) {
+      this.logger.error([`interaction.member was null somehow`, interaction]);
+      throw new Error(`interaction.member was null somehow`);
+    } else if (interaction.guild == null) {
+      this.logger.error([`interaction.guild was null somehow`, interaction]);
+      throw new Error(`interaction.guild was null somehow`);
+    }
+
+    const guild_ent = await this.guilds.findOneOrFail({
+      id: interaction.guild.id,
+    });
+
+    const intro_songs = await this.intro_songs.find({
+      public: true,
+      duration_ms: {
+        $lte: secondsToMilliseconds(
+          Math.min(
+            guild_ent.intro.max_duration_s,
+            configuration.audio.custom_intro.max_dur_s,
+          ),
+        ),
+      },
+    });
+
+    return interaction.respond(
+      intro_songs.map<ApplicationCommandOptionChoiceData<string | number>>(
+        (song) => ({ name: song.name, value: song.hash }),
+      ),
+    );
   }
 }
