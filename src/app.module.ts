81d1b080d2a92fa54fc8a546b8aa91f75d990bfb
Louis Orleans <louis@orleans.io>
2023-02-16 03:53:51 -0800

    🔥 remove default controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BotModule } from './bot/bot.module';
 import { DatabaseModule } from './database/database.module';
 import { SchedulingModule } from './scheduling/scheduling.module';
@@ -15,7 +13,5 @@ import { StorageModule } from './storage/storage.module';
     BotModule,
     SchedulingModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
