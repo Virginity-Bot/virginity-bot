c47b2b0d36f3442cf7ddea1f61183cdce8d7c106
Louis Orleans <louis@orleans.io>
2023-02-17 02:12:17 -0800

    ✨ make bucket creation command more resilient
    - waits for MinIO to be available
    - verifies that the bucket was created or already existed
    - supports args (with defaults)

--- files/object-storage/create-bucket.sh
+++ files/object-storage/create-bucket.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 set -e
 
-username="admin"
-password="password"
+origin="${1:-http://object-storage:9000}"
+username="${2:-admin}"
+password="${3:-password}"
+
+max_retries=2
 
 # ensure the `mc` bin is available
 if ! type "mc" > /dev/null; then
@@ -13,7 +16,34 @@ if ! type "mc" > /dev/null; then
 fi
 
 # wait for MinIO to come up
-sleep 5
+retries=0
+set +e;
+while [ true ]; do
+  curl --silent --url "$origin" > /dev/null
+  if [ "$?" -eq 0 ]; then
+    break
+  else
+    retries=$((retries+1))
+    if [ "$retries" -eq "$max_retries" ]; then
+      exit 1
+    else
+      sleep 2
+    fi
+  fi
+done
+set -e;
 
-mc alias set vbot http://object-storage:9000 "$username" "$password"
-mc mb vbot/intro-songs
+mc alias set vbot "$origin" "$username" "$password" > /dev/null
+
+set +e; output="$(mc mb vbot/intro-songs 2>&1)"; set -e
+
+if \
+  [[ "$output" =~ "Your previous request to create the named bucket succeeded" ]] || \
+  [[ "$output" =~ "Bucket created successfully" ]] \
+; then
+  exit 0
+else
+  echo "Failed to create bucket."
+  echo "$output"
+  exit 1
+fi
