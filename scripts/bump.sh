d8439afed3aa9530ae8a3e5403fa8cdb28910b6f
EdgarSaldivar <edgarls93@hotmail.com>
2023-02-17 03:26:19 -0800

    🐛 Fixed Quotes bug

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -14,9 +14,9 @@ fi
 if ! [[ $1 =~ $re ]] ; then
   if [[ $list =~ (^|[[:space:]])$1($|[[:space:]]) ]] ; then
     if [[ ! -z "$2" ]] ; then
-      npm version $1 -m $message
+      npm version $1 -m "$message"
     else
-      npm version $1 $2 -m $message
+      npm version $1 $2 -m "$message"
     fi
   fi
 fi
