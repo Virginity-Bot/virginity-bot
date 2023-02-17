bcddff280aad4df27ae67bf18577d4fcb2c02ff3
EdgarSaldivar <edgarls93@hotmail.com>
2023-02-17 03:36:45 -0800

    🗑️ Code cleanup

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -4,7 +4,6 @@ echo "Bumping... "
 re='^[0-9]+$'
 list='major minor patch'
 message='🚀🔖 release v%s'
-echo $message
 
 if [[ -z "$1" ]] ; then
       npm version --message "$message" patch
@@ -13,10 +12,8 @@ fi
 
 if ! [[ $1 =~ $re ]] ; then
   if [[ $list =~ (^|[[:space:]])$1($|[[:space:]]) ]] ; then
-    if [[ ! -z "$2" ]] ; then
-      npm version $1 -m "$message"
+    npm version $1 -m "$message"
     else
-      npm version $1 $2 -m "$message"
-    fi
+    npm version $1 -m "$message"
   fi
 fi
