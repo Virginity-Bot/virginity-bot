b771fc0516034b36012ef210c4c0d7a7759d5582
Louis Orleans <louis@orleans.io>
2023-02-16 23:00:03 -0800

    🚚 rename prometheus logs

--- src/prometheus/prometheus.service.ts
+++ src/prometheus/prometheus.service.ts
@@ -1,20 +1,26 @@
 import { hostname } from 'node:os';
 
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
-import { Gauge, Histogram, Counter, collectDefaultMetrics } from 'prom-client';
+import {
+  Gauge,
+  Histogram,
+  Counter,
+  collectDefaultMetrics,
+  exponentialBuckets,
+} from 'prom-client';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class PrometheusService {
   /** Request response times in seconds. */
   response_time_s = new Histogram({
-    name: 'response_time_seconds',
+    name: 'vbot_response_time_seconds',
     help: 'Request response times.',
   });
 
   /** Total count of guilds. */
   guild_count = new Gauge({
-    name: 'guild_count',
+    name: 'vbot_guild_count',
     help: 'Total count of guilds.',
     collect: async (): Promise<void> => {
       this.guild_count.set(await this.database.getGuildCount());
@@ -23,7 +29,7 @@ export class PrometheusService {
 
   /** Total count of users. */
   user_count = new Gauge({
-    name: 'user_count',
+    name: 'vbot_user_count',
     help: 'Total count of users.',
     collect: async (): Promise<void> => {
       this.user_count.set(await this.database.getUserCount());
@@ -33,7 +39,7 @@ export class PrometheusService {
   // TODO: Should this be a Counter? Kinda feels like it, but makes the API more awkward
   /** Total count of vc_events. */
   vc_event_count = new Gauge({
-    name: 'vc_event_count',
+    name: 'vbot_vc_event_count',
     help: 'Total count of vc_events.',
     collect: async (): Promise<void> => {
       this.vc_event_count.set(await this.database.getVCEventCount());
@@ -42,13 +48,14 @@ export class PrometheusService {
 
   /** Duration of closed VC events in seconds. */
   vc_event_duration_s = new Histogram({
-    name: 'vc_event_duration_seconds',
+    name: 'vbot_vc_event_duration_seconds',
     help: 'Duration of closed VC events.',
+    buckets: exponentialBuckets(1, 4, 10),
   });
 
   /** Total count of unclosed vc_events. */
   unclosed_vc_event_count = new Gauge({
-    name: 'unclosed_vc_event_count',
+    name: 'vbot_unclosed_vc_event_count',
     help: 'Total count of unclosed vc_events.',
     collect: async (): Promise<void> => {
       this.unclosed_vc_event_count.set(
@@ -61,6 +68,6 @@ export class PrometheusService {
     @Inject(forwardRef(() => DatabaseService))
     private readonly database: DatabaseService,
   ) {
-    // collectDefaultMetrics({ labels: { NODE_APP_INSTANCE: hostname() } });
+    collectDefaultMetrics({ labels: { NODE_APP_INSTANCE: hostname() } });
   }
 }
