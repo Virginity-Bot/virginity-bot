a4668df68eec46c4e2b2f3deee2c7817d1223666
EdgarSaldivar <edgarls93@hotmail.com>
2023-02-17 03:44:11 -0800

    🗑️ Code cleanup - removed allot of unecesary code

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Bumping... "
-re='^[0-9]+$'
-list='major minor patch'
-message='🚀🔖 release v%s'
 
 if [[ -z "$1" ]] ; then
       npm version --message "$message" patch
       exit 1
-fi
-
-if ! [[ $1 =~ $re ]] ; then
-  if [[ $list =~ (^|[[:space:]])$1($|[[:space:]]) ]] ; then
-    npm version $1 -m "$message"
-    else
-    npm version $1 -m "$message"
-  fi
+else
+  npm version $1 -m "$message"
+  exit 1
 fi
