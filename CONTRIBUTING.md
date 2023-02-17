3ce3ce9d4df4b8613d28930468fd6deec7641d07
Louis Orleans <louis@orleans.io>
2023-02-16 20:07:11 -0800

    🔧✨ add Prometheus container and configure Grafana

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -22,11 +22,6 @@ What a gamer! We'd love to see where you want to take Virginity Bot!
     1. [Create a new bucket](http://localhost:9001/buckets/add-bucket) named `intro-songs`.
     1. [Create a new access key](http://localhost:9001/access-keys/new-account) and add the values to your `/.env`.
 
-1. Setup Grafana
-
-    1. Go to http://localhost:3000/datasources and login with the root credentials in the [`docker-compose.dev.yaml` file](/docker-compose.dev.yaml).
-    1. Add a Loki datasource and set the URL to `http://loki:3100`
-
 1. Connect your debugger.
 
     The server container forwards port `9229`, so you should be able to connect a NodeJS debugger there.
---
+++ files/grafana/add-data-sources.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+username="admin"
+password="password"
+
+# wait for Grafana to come up
+sleep 5
+
+curl --request POST \
+  --url http://$username:$password@grafana:3000/api/datasources \
+  --header 'Content-Type: application/json' \
+  --data '{
+    "name":"Loki",
+    "type":"loki",
+    "url":"http://loki:3100",
+    "access":"proxy",
+    "basicAuth":false
+  }'
+
+curl --request POST \
+  --url http://$username:$password@grafana:3000/api/datasources \
+  --header 'Content-Type: application/json' \
+  --data '{
+    "name":"Prometheus",
+    "type":"prometheus",
+    "url":"http://prometheus:9090",
+    "access":"proxy",
+    "basicAuth":false
+  }'
