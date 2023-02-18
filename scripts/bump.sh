0fad7514ed0626dbe66a4d85300a85678e1beb7d
EdgarSaldivar <edgarls93@hotmail.com>
2023-02-17 16:53:46 -0800

    🐛 FIxed Indents

---
+++ scripts/bump.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Bumping... "
+message='🚀🔖 release v%s'
+
+if [[ -z "$1" ]] ; then
+  npm version --message "$message" patch
+  exit 0
+else
+  npm version --message "$message" $1
+  exit 0
+fi
