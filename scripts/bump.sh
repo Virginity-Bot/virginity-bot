7a766b725c7421d91506c8215f01b0ce238a681c
EdgarSaldivar <edgarls93@hotmail.com>
2023-02-17 16:44:28 -0800

    🐛 Fixed Bugs

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 echo "Bumping... "
+message='🚀🔖 release v%s'
 
 if [[ -z "$1" ]] ; then
       npm version --message "$message" patch
       exit 1
 else
-  npm version $1 -m "$message"
+  npm version --message "$message" $1
   exit 1
 fi
