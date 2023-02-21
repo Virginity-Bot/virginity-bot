a461b8f0665ce1c17c5cc1707d9d00879c84b820
Louis Orleans <louis@orleans.io>
2023-02-21 01:10:16 -0800

    ✅ update tests with new intended behavior

--- .github/workflows/scripts/test/get-image-tags.unit.rb
+++ .github/workflows/scripts/test/get-image-tags.unit.rb
@@ -1,12 +1,13 @@
 require 'test/unit'
 require 'json'
+require 'set'
 
 require_relative '../lib'
 
 class TestGetImageTags < Test::Unit::TestCase
   def test_get_image_tags
     assert_equal(
-      ['ghcr.io/virginity-bot/virginity-bot/bot:feat-foo-bar'],
+      Set['ghcr.io/virginity-bot/virginity-bot/bot:feat-foo-bar'],
       get_image_tags(
         git_repo: 'Virginity-Bot/virginity-bot',
         git_ref_name: 'feat/foo-bar',
@@ -17,10 +18,9 @@ class TestGetImageTags < Test::Unit::TestCase
     )
 
     assert_equal(
-      %w[
-        ghcr.io/virginity-bot/virginity.bot/bot:1.0.0
-        ghcr.io/virginity-bot/virginity.bot/bot:latest
-        ghcr.io/virginity-bot/virginity.bot/bot:master
+      Set[
+        'ghcr.io/virginity-bot/virginity.bot/bot:latest',
+        'ghcr.io/virginity-bot/virginity.bot/bot:master'
       ],
       get_image_tags(
         git_repo: 'Virginity-Bot/virginity.bot',
@@ -32,25 +32,28 @@ class TestGetImageTags < Test::Unit::TestCase
     )
 
     assert_equal(
-      ['ghcr.io/virginity-bot/virginity.bot/bot:feat-foo-bar'],
+      Set[
+        'ghcr.io/virginity-bot/virginity.bot/bot:latest',
+        'ghcr.io/virginity-bot/virginity.bot/bot:master',
+        'ghcr.io/virginity-bot/virginity.bot/bot:1.0.0',
+        'ghcr.io/virginity-bot/virginity.bot/bot:1.0',
+        'ghcr.io/virginity-bot/virginity.bot/bot:1'
+      ],
       get_image_tags(
         git_repo: 'Virginity-Bot/virginity.bot',
-        git_ref_name: 'feat/Foo---bar',
-        git_ref_type: 'branch',
+        git_ref_name: '1.0.0',
+        git_ref_type: 'tag',
         git_default_branch: 'master',
         package: JSON.parse('{"version": "1.0.0"}'),
       ),
     )
 
     assert_equal(
-      %w[
-        ghcr.io/virginity-bot/virginity.bot/bot:1.0.0
-        ghcr.io/virginity-bot/virginity.bot/bot:latest
-      ],
+      Set['ghcr.io/virginity-bot/virginity.bot/bot:feat-foo-bar'],
       get_image_tags(
         git_repo: 'Virginity-Bot/virginity.bot',
-        git_ref_name: '1.0.0',
-        git_ref_type: 'tag',
+        git_ref_name: 'feat/Foo---bar',
+        git_ref_type: 'branch',
         git_default_branch: 'master',
         package: JSON.parse('{"version": "1.0.0"}'),
       ),
