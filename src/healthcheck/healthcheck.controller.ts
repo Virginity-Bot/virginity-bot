23ee9dca894ab8e687c996ff2bcadf302cebad68
Louis Orleans <louis@orleans.io>
2023-03-03 16:24:30 -0800

    ✨ add more discord info to healthcheck

--- src/healthcheck/healthcheck.controller.ts
+++ src/healthcheck/healthcheck.controller.ts
@@ -7,13 +7,46 @@ export class HealthcheckController {
   constructor(private readonly healthcheck: HealthcheckService) {}
 
   @Get()
-  async health(): Promise<{ discord: boolean }> {
-    const tests = { discord: this.healthcheck.checkDiscordWS() };
+  async health(): Promise<UptimeResponse> {
+    const tests: UptimeResponse = {
+      discord: {
+        healthy: this.healthcheck.checkDiscordWS(),
+        status: this.healthcheck.getDiscordWSStatus(),
+        uptime_s: this.healthcheck.getDiscordUptime(),
+        ready_at: this.healthcheck.getDiscordReadyAt(),
+        ping_s: this.healthcheck.getDiscordPing(),
+      },
+    };
 
-    if (Object.values(tests).some((v) => !v)) {
+    if (Object.values(tests).some((test) => this.checkTest(test))) {
       throw new InternalServerErrorException(tests);
     } else {
       return tests;
     }
   }
+
+  checkTest(test: unknown): boolean {
+    switch (typeof test) {
+      case 'object':
+        if (test != null) {
+          return Object.values(test).some((test) => this.checkTest(test));
+        } else {
+          return true;
+        }
+      case 'boolean':
+        return !test;
+      default:
+        return true;
+    }
+  }
+}
+
+export interface UptimeResponse {
+  discord: {
+    healthy: boolean;
+    status: string;
+    uptime_s: number | null;
+    ready_at: Date | null;
+    ping_s: number;
+  };
 }
