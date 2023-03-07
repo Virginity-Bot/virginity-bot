10043c84f3e2df383496d17dcc67e484c9024d0c
EdgarSaldivar <edgarls93@hotmail.com>
2023-03-07 15:46:14 -0800

    Rules Command (#170)

---
+++ src/bot/commands/rules.command.ts
@@ -0,0 +1,60 @@
+import {
+  Injectable,
+  Logger,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Command, EventParams, Handler } from '@discord-nestjs/core';
+import {
+  CommandInteraction,
+  MessagePayload,
+  PermissionFlagsBits,
+} from 'discord.js';
+import { MikroORM, UseRequestContext } from '@mikro-orm/core';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { EntityRepository } from '@mikro-orm/postgresql';
+
+import { GuildEntity } from 'src/entities/guild/guild.entity';
+import { RulesService } from '../rules.service';
+import { TimingLogInterceptor } from '../interceptors/logging.interceptor';
+import { CatchallErrorFilter } from '../filters/catchall-error.filter';
+
+@Command({
+  name: 'rules',
+  description: `Rules for Virginity Bot on your server.`,
+  defaultMemberPermissions: PermissionFlagsBits.SendMessages,
+  dmPermission: false,
+})
+@Injectable()
+@UseFilters(CatchallErrorFilter)
+@UseInterceptors(TimingLogInterceptor)
+export class RulesCommand {
+  private readonly logger = new Logger(RulesCommand.name);
+
+  constructor(
+    private readonly orm: MikroORM,
+    @InjectRepository(GuildEntity)
+    private readonly guilds: EntityRepository<GuildEntity>,
+    private readonly rules: RulesService,
+  ) {}
+
+  @Handler()
+  @UseRequestContext()
+  async handler(
+    @EventParams() [interaction]: [interaction: CommandInteraction],
+  ): Promise<MessagePayload> {
+    if (interaction.guildId == null || interaction.guild == null) {
+      this.logger.error([`interaction.guildId was null somehow`, interaction]);
+      throw new Error(`interaction.guildId was null somehow`);
+    } else if (interaction.channel == null) {
+      this.logger.error([`interaction.channel was null somehow`, interaction]);
+      throw new Error(`interaction.channel was null somehow`);
+    }
+
+    const guild_ent = await this.guilds.findOneOrFail(interaction.guildId);
+
+    const rulesBoard = await this.rules.buildRulesboardEmbed(guild_ent);
+
+    return new MessagePayload(interaction.channel, { embeds: [rulesBoard] });
+  }
+}
